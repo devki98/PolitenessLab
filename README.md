3773fe8bbbe47c08fe462b1635681c6bedc1a716
Devki Trivedi <devki98@gmail.com>
2018-03-08 17:19:54 -0500

    vaderSentiment online text analyzer using flask
    http://127.0.0.1:5000/form-example

--- README.md
+++
@@ -1,3 +0,0 @@
-# PolitenessLab
-
-after running app.py, to use form go to : http://127.0.0.1:5000/form-example
--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, request, render_template
+#app.py
 
-app = Flask(__name__)
+from flask import Flask, request #import main Flask class and request object
+app = Flask(__name__) #create the Flask app
+from vaderSentiment import analyse_sentence
 
-@app.route('/')
-def my_form():
-    return render_template('hello.html')
+@app.route('/query-example')
+def query_example():
+    sentence = request.args.get('sentence') #if key doesn't exist, returns None
+    #sentiment = request.args['sentiment'] #if key doesn't exist, returns a 400, bad request error
+    #website = request.args.get('website')
+    sent = analyse_sentence(sentence)
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    return '''<h1>The sentence value is: {}</h1>
+
+              <h1>The website value is: {}'''.format(sentence)
+
+@app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
+def form_example():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        sentence = request.form.get('sentence')
+        #sentiment = request.form['sentiment']
+        sent = analyse_sentence(sentence)
+        return '''<h1>The sentence value is: {}</h1>
+                  <h1>The sentiment value is: {}</h1>'''.format(sentence, sent )
+
+    return '''<form method="POST">
+                  Sentence: <input type="text" name="sentence"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000) #run app in debug mode on port 5000
